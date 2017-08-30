3933a18763439e426f24ba88b191e95d33670664
Nadir Fayazov <nfayazov@gmail.com>
2017-08-29 21:32:11 -0700

    Parse headers

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var app = express()
 
 app.get('/', function(req, res) {
    res.json({
-      ipaddress: req.ip
+      ipaddress: req.headers['x-forwarded-for'],
+      language: req.headers["accept-language"].split(',')[0],
+      software: req.headers['user-agent'].split(/[()]+/)[1]
    })
 })
 
-app.listen("3000", function() {
+app.listen("process.env.PORT", function() {
    console.log("It's working")
 })
